e4b8b56905f92dc1fe50040480df24996392b92f
Konstantin Bukin <postoyanstvo@yahoo.com>
2023-06-12 14:11:34 +0300

    Created basic counter functional: label with value of counter, button that increases value by one

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var addButton: UIButton!
+    private var counterValue: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Set initial values and font size of button text
+        countLabel.text = "0"
+        addButton.titleLabel?.text = "+ 1"
+        addButton.titleLabel?.font = UIFont.systemFont(ofSize: 50)
     }
 
 
+    @IBAction func addOneToCounter(_ sender: Any) {
+        // Increase counter value by 1 and show value by label
+        counterValue += 1
+        countLabel.text = "Значение счётчика: \(counterValue)"
+    }
 }
 
