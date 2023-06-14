f23c807a3f08c7a5d3dc52e5c85e12f010bf9abb
Konstantin Bukin <postoyanstvo@yahoo.com>
2023-06-14 09:40:37 +0300

    Added scrolling a textView if lines added. Prettified ui: added background gradient, changed locations, shapes, colors of elements

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var deductButton: UIButton!
     @IBOutlet weak var resetButton: UIButton!
     @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var backgroundGradientView: UIView!
     
     private var counterValue: Int = 0
     private var currentDateTimeStamp: String {
@@ -24,36 +25,59 @@ class ViewController: UIViewController {
         return dateFormatter.string(from: date)
     }
     
-
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Set initial properties
         countLabel.text = "0"
         
-        addButton.tintColor = .red
         addButton.titleLabel?.text = "+"
-        addButton.titleLabel?.font = UIFont.systemFont(ofSize: 50)
+        addButton.titleLabel?.font = UIFont.systemFont(ofSize: 100)
         
         deductButton.titleLabel?.text = "-"
-        deductButton.titleLabel?.font = UIFont.systemFont(ofSize: 50)
+        deductButton.titleLabel?.font = UIFont.systemFont(ofSize: 100)
         
         resetButton.titleLabel?.text = "0"
-        resetButton.titleLabel?.font = UIFont.systemFont(ofSize: 50)
+        resetButton.titleLabel?.font = UIFont.systemFont(ofSize: 45)
         
         textView.text = "История изменений:" + "\n"
-        textView.layer.borderWidth = 0.5
-        textView.clipsToBounds = true
-        textView.layer.cornerRadius = 10.0
+        
+        setBackground()
     }
+    
+    // Set gradient background
+    private func setBackground() {
+        view.backgroundColor = .white
+        navigationItem.title = "Gradient View"
 
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [#colorLiteral(red: 0.9764705882, green: 0.5960784314, blue: 0.2901960784, alpha: 0.4039993791).cgColor, #colorLiteral(red: 0.9764705882, green: 0.7568627451, blue: 0.2901960784, alpha: 0.1489807533).cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+
+        gradientLayer.frame = view.frame
+        backgroundGradientView.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    // Scroll text in textView if added new line
+    func scrollTextViewToBottom(textView: UITextView) {
+        if textView.text.count > 0 {
+            let location = textView.text.count - 1
+            let bottom = NSMakeRange(location, 1)
+            textView.scrollRangeToVisible(bottom)
+        }
+    }
+    
+    // Actions with bottons
     @IBAction func addOneToCounter(_ sender: Any) {
         // Increase counter value by 1 and show value by label
         counterValue += 1
         countLabel.text = "Значение счётчика: \(counterValue)"
         textView.text += "\(currentDateTimeStamp): значение изменено на +1" + "\n"
+        scrollTextViewToBottom(textView: textView)
     }
+    
     @IBAction func deductOneFromCounter(_ sender: Any) {
         // Decrease counter value by 1 and show value by label
         if counterValue > 0 {
@@ -63,6 +87,7 @@ class ViewController: UIViewController {
         } else {
             textView.text += "\(currentDateTimeStamp): попытка уменьшить значение счётчика ниже 0" + "\n"
         }
+        scrollTextViewToBottom(textView: textView)
     }
     
     @IBAction func resetCounter(_ sender: Any) {
@@ -70,6 +95,7 @@ class ViewController: UIViewController {
         counterValue = 0
         countLabel.text = "0"
         textView.text += "\(currentDateTimeStamp): значение сброшено" + "\n"
+        scrollTextViewToBottom(textView: textView)
     }
 }
 
